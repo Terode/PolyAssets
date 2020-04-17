bee26cb6bed812c6d150fe4e8e5d26cee44f126c
Alex <alex.terode@gmail.com>
2020-04-17 16:16:49 +0300

    * create project

---
+++ PolyAssets/PolyAssets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PolyAssets
+//
+//  Created by Alex Sorokolita on 17.04.2020.
+//  Copyright © 2020 Alex Sorokolita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
